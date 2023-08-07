f9121b9eb9673c100b1c950ed3eda16c51323453
YJWang94108 <paul94108@gmail.com>
2023-08-07 21:51:22 +0800

    add homepage

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Fantasy Baseball</h1>Hello, World!"
+    return render_template("home.html")
 
 if __name__ == '__main__':
     app.run()
