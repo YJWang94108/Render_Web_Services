b72a73003d90bc34b342c1f0881d8dcb460c7d28
YJWang94108 <54131193+YJWang94108@users.noreply.github.com>
2024-01-24 21:20:41 +0800

    Add server.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+from server import Test
 
-app = Flask(__name__, static_folder="static")
+app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    Test()
     return render_template("home.html")
 
 if __name__ == '__main__':
