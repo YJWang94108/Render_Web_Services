256e24d2d97991ee93703c4c1024417bce519632
YJWang94108 <54131193+YJWang94108@users.noreply.github.com>
2024-01-25 22:02:20 +0800

    update app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+
+
+@socketio.on("message")
+def sendMessage(message):
+    send(message, broadcast=True)
+    # send() function will emit a message vent by default
+
 
 @app.route("/")
 def hello():
     return render_template("home.html")
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
     
