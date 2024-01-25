4694e2df63c9400df72065727f866cc0c593c161
YJWang94108 <54131193+YJWang94108@users.noreply.github.com>
2024-01-25 21:01:29 +0800

    update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from server import Start
 
 app = Flask(__name__)
 
@@ -8,6 +7,5 @@ def hello():
     return render_template("home.html")
 
 if __name__ == '__main__':
-    #Start()
     app.run()
     
