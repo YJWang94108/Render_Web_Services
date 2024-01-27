8430f9da044a80802684d25507b70927c446b3a7
YJWang94108 <54131193+YJWang94108@users.noreply.github.com>
2024-01-27 14:53:27 +0800

    Testing server.py

--- server.py
+++ server.py
@@ -1,8 +1,23 @@
 import socket
 
+DEFINE = {
+    'HOST': '127.0.0.1',
+    'PORT': None,
+    'Connection': None,
+    'Timeout': 30,
+    'LaunchState': 0,
+    'TextLength': 1024*4
+}
+
+def log(msg:str):
+    print(f"[PYTHON]: {msg}", flush=True)
+
 def Start():
-    print(">> Start", flush=True)
+    global DEFINE
+
+    log(">> Start")
     hostname = socket.gethostname()
     local_ip = socket.gethostbyname(hostname)
-    print(f"IP: {str(local_ip)}", flush=True)
+    log(f"IP: {str(local_ip)}")
+    print('>>', DEFINE['Timeout'])
     
