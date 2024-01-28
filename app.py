232c445c6496e0c98a14c2cc2bb99a7d245ad8bf
YJWang94108 <54131193+YJWang94108@users.noreply.github.com>
2024-01-28 15:36:23 +0800

    Testing

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
+
 
 @app.route("/Test")
 def index():
@@ -19,4 +19,5 @@ def hello():
 
 ## Main
 app.run('0.0.0.0', debug=True)
+socketio = SocketIO(app, cors_allowed_origins="*")
     
