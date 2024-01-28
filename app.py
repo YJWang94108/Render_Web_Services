a91c079717664711d851d6b7443365207c922719
YJWang94108 <54131193+YJWang94108@users.noreply.github.com>
2024-01-28 15:46:54 +0800

    debug app.py

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-
-@app.route("/Test")
-def index():
-    return render_template("index.html")
-    
 @socketio.on('message')
 def handle_message(msg):
     print('Message:', msg)
     socketio.emit('message', msg, broadcast=True)
+
+@app.route("/Test")
+def index():
+    return render_template("index.html")
 
 @app.route("/")
 def hello():
@@ -19,5 +19,4 @@ def hello():
 
 ## Main
 app.run('0.0.0.0', debug=True)
-socketio = SocketIO(app, cors_allowed_origins="*")
     
