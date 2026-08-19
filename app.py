e90b568c2450fca58fc422d304cb922929d4b7f3
YJWang94108 <paul94108@gmail.com>
2026-08-19 11:46:09 +0800

    Add a home page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from flask import request, redirect, url_for
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
@@ -17,12 +18,38 @@ def handle_message(msg):
     emit('message', msg, broadcast=True)
 
 @app.route("/Test")
-def index():
+def testing():
     return render_template("index.html")
 
+# ======================================
+# Home Page
+# /
+#
 @app.route("/")
-def hello():
+def index():
     return render_template("home.html")
+
+# ======================================
+# Login Page
+# /login
+#
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "GET":
+        return render_template("login.html")
+
+    username = request.form.get("username")
+    if not username:
+        return render_template(
+            "login.html",
+            error="請輸入玩家名稱"
+        )
+
+    # Print Login Information
+    log(f"[LOGIN] Player logged in: {username}")
+
+    return redirect(url_for("index"))
 
 ## Main
 
